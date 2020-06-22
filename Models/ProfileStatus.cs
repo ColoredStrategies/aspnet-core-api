94124e0b752e1d512689d675a0366411787e3e1e
originalai <sinanisil@gmail.com>
2020-06-22 04:41:42 +0300

    ProfileStatus model added

---
+++ Models/ProfileStatus.cs
@@ -0,0 +1,10 @@
+namespace aspnet_core_api.Models
+{
+    public class ProfileStatus
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public int total { get; set; }
+        public int status { get; set; }
+    }
+}
