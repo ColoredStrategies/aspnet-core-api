f554889ca8db15c441d0dceaf38e57798ee1416f
originalai <sinanisil@gmail.com>
2020-06-22 04:39:37 +0300

    comment model added

---
+++ Models/Comment.cs
@@ -0,0 +1,10 @@
+namespace aspnet_core_api.Models
+{
+    public class Comment
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string detail { get; set; }
+        public string img { get; set; }
+    }
+}
