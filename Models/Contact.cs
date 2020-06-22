48380d442ff30c3b133757f22b197fe6c6a1ca02
originalai <sinanisil@gmail.com>
2020-06-22 04:42:26 +0300

    Contact model added

---
+++ Models/Contact.cs
@@ -0,0 +1,10 @@
+namespace aspnet_core_api.Models
+{
+    public class Contact
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string img { get; set; }
+        public string date { get; set; }
+    }
+}
