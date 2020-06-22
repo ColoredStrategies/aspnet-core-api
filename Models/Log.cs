bff17b96988f24f45ded8fdf123d7af6e255aebe
originalai <sinanisil@gmail.com>
2020-06-22 04:40:19 +0300

    log model added

---
+++ Models/Log.cs
@@ -0,0 +1,11 @@
+namespace aspnet_core_api.Models
+{
+  public class Log
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string date { get; set; }
+        public string color { get; set; }
+
+    }
+}
